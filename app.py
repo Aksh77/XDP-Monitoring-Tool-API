b3be64a24e00ea40449a52bc86d3c46732a49797
Akshatha Nayak <akshatha@ebi.ac.uk>
2022-07-17 14:29:38 +0100

    fixing cors issue

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import os
 import csv
-import itertools
-import operator
   
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/patient/<id>')
 def get_patient_data(id):
