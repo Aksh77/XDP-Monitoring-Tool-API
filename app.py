97f9fc80215568236bc211ff6184e10615bfe7a1
Akshatha Nayak <akshatha@ebi.ac.uk>
2022-07-17 15:39:49 +0100

    fixed grouping

--- app.py
+++ app.py
@@ -7,6 +7,30 @@ import csv
 app = Flask(__name__)
 CORS(app)
 
+def group_by_test_type(data):
+    data_grouped = {}
+    for obj in data:
+        if obj["Test type"] in data_grouped:
+            data_grouped[obj["Test type"]]["Sub-tests"].append({
+                "Test parameter": obj["Test parameter"],
+                "Score": obj["Score"],
+                "Unit": obj["Unit"]
+            })
+        else:
+            data_grouped[obj["Test type"]] = {
+                "Scale": obj["Scale"],
+                "Status": obj["Status"],
+                "Test type": obj["Test type"],
+                "Video": obj["Video"],
+                "Sub-tests": [{
+                    "Test parameter": obj["Test parameter"],
+                    "Score": obj["Score"],
+                    "Unit": obj["Unit"]
+                }]
+            }
+    data_grouped = list(data_grouped.values())
+    return data_grouped
+
 @app.route('/patient/<id>')
 def get_patient_data(id):
     if id == '123':
@@ -37,19 +61,20 @@ def get_patient_data(id):
                     }
                     data.append(obj)
         # group by date
-        data_grouped_by_date = {}
+        grouped_by_date = {}
         for obj in data:
-            if obj["Date"] in data_grouped_by_date:
-                data_grouped_by_date[obj["Date"]]["Tests"].append({
+            if obj["Date"] in grouped_by_date:
+                grouped_by_date[obj["Date"]]["Tests"].append({
                     "Scale": obj["Scale"],
                     "Status": obj["Status"],
                     "Test type": obj["Test type"],
                     "Video": obj["Video"],
                     "Test parameter": obj["Test parameter"],
-                    "Score": obj["Score"]
+                    "Score": obj["Score"],
+                    "Unit": obj["Unit"]
                 })
             else:
-                data_grouped_by_date[obj["Date"]] = {
+                grouped_by_date[obj["Date"]] = {
                     "Date": obj["Date"],
                     "Medicine": obj["Medicine"],
                     "Tests": [{
@@ -58,34 +83,11 @@ def get_patient_data(id):
                         "Test type": obj["Test type"],
                         "Video": obj["Video"],
                         "Test parameter": obj["Test parameter"],
-                        "Score": obj["Score"]
-                    }]
-                }
-        data_grouped_by_date = list(data_grouped_by_date.values())
-
-        def group_by_test_type(obj):
-            data_grouped = {}
-            for obj in data:
-                if obj["Test type"] in data_grouped:
-                    data_grouped[obj["Test type"]]["Sub-tests"].append({
-                        "Test parameter": obj["Test parameter"],
                         "Score": obj["Score"],
                         "Unit": obj["Unit"]
-                    })
-                else:
-                    data_grouped[obj["Test type"]] = {
-                        "Scale": obj["Scale"],
-                        "Status": obj["Status"],
-                        "Test type": obj["Test type"],
-                        "Video": obj["Video"],
-                        "Sub-tests": [{
-                            "Test parameter": obj["Test parameter"],
-                            "Score": obj["Score"],
-                            "Unit": obj["Unit"]
-                        }]
-                    }
-            data_grouped = list(data_grouped.values())
-            return data_grouped
+                    }]
+                }
+        data_grouped_by_date = list(grouped_by_date.values())
 
         # group by date and test type
         data_grouped_by_date_test_type = []
@@ -93,10 +95,9 @@ def get_patient_data(id):
             data_grouped_by_test_type = {
                 "Date": obj["Date"],
                 "Medicine": obj["Medicine"],
-                "Tests": group_by_test_type(obj)
+                "Tests": group_by_test_type(obj['Tests'])
             }
             data_grouped_by_date_test_type.append(data_grouped_by_test_type)
-
         return {"data": data_grouped_by_date_test_type}
     else:
         return "Patient not found"
